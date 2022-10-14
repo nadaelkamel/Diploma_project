0856bb736d2a463427f9e7659191a04a22faa7a0
nada elkamel <95017795+nadaelkamel@users.noreply.github.com>
2022-10-14 18:14:02 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+/*
+ * main.c
+
+ *
+ *  Created on: Aug 23, 2022
+ *      Author: nadakenzyali
+ */
+
+#include "Projects.h"
+
+void A_EXT_INT0_Execution(void);
+void A_Timer0_Execution(void);
+void A_Timer1_Execution(void);
+
+int main()
+{
+
+	return 0;
+}
+
+void A_EXT_INT0_Execution(void)
+{
+
+}
+
+void A_Timer0_Execution(void)
+{
+
+}
+void A_Timer1_Execution(void)
+{
+
+}
+
+
+
+
+
